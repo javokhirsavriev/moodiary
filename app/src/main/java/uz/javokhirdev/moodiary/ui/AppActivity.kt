061127d11bcabe5828dba12ab11dad208ef014c9
Javokhir <javokhir@Javokhirs-MacBook-Pro.local>
2021-11-08 03:02:18 +0500

    initial commit

---
+++ app/src/main/java/uz/javokhirdev/moodiary/ui/AppActivity.kt
@@ -0,0 +1,12 @@
+package uz.javokhirdev.moodiary.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.javokhirdev.moodiary.R
+
+class AppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+    }
+}
